5f4815e1cb77186fd68cf967095ed9a153645b88
Nicolas Charpentier <nicolascharpentier@MacBook-Pro-de-Nicolas.local>
2018-01-26 15:25:39 +0100

    Exercise 4

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 let chalk = require('chalk');
 
 const app = require('./src/app');
-
+var fs = require('fs')
+var file = "numbers.txt"
 // port
 const port = 3000;
 
 // start server here
 app.get('/bingo', function(req, res) {
+    var nb = "";
+    fs.readFile(file, function (err, data) {
+    nb = data.toString().split('\n').join(",");
+    console.log(nb);
+    });
+    res.send(nb);
     res.end('Bingo')
 })
 app.listen( process.argv[2] || port)
