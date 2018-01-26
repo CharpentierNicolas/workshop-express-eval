04f8728fc9384d4f2d4a6dfd27b1656f6f21afaf
Nicolas Charpentier <nicolascharpentier@MacBook-Pro-de-Nicolas.local>
2018-01-26 15:05:21 +0100

    Exercise 3

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const app = require('./src/app');
 const port = 3000;
 
 // start server here
+app.get('/bingo', function(req, res) {
+    res.end('Bingo')
+})
 app.listen( process.argv[2] || port)
 console.log(chalk.green('Hello web server'));
