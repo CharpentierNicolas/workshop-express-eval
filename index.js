17a9a1fbfe6eedf35d5d77a754a11f60e077865b
Nicolas Charpentier <nicolascharpentier@MacBook-Pro-de-Nicolas.local>
2018-01-26 15:39:28 +0100

    Correction Exercise 4

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const port = 3000;
 
 // start server here
 app.get('/bingo', function(req, res) {
-    var nb = "";
+    var nb = [];
     fs.readFile(file, function (err, data) {
-    nb = data.toString().split('\n').join(",");
+    nb.push(data.toString().split('\n').join(","));
     console.log(nb);
-    });
     res.send(nb);
-    res.end('Bingo')
+    });
+
+    //res.end('Bingo' + nb + bn)
 })
 app.listen( process.argv[2] || port)
 console.log(chalk.green('Hello web server'));
